c361294fba153baa4c82dba7dddb4534ee090fd7
Michael Romashov <mromashov@icloud.com>
2023-11-30 14:03:13 -0500

    Optimize population generation

--- election/main.py
+++ election/main.py
@@ -1,7 +1,7 @@
 import itertools
+import time
 
 import numpy as np
-import numpy.typing as np_type
 
 from election.demographics import Demographics
 
@@ -9,33 +9,33 @@ from election.demographics import Demographics
 # Therefore, we'll construct a joint probability mass function.
 # Probabilities are listed below in the same order as they appear in `Demographics`.
 #
-# Source:
+# Sources:
 # https://www.census.gov/data/tables/2022/demo/age-and-sex/2022-age-sex-composition.html
+# https://www.pewresearch.org/social-trends/2018/05/22/demographic-and-economic-trends-in-urban-suburban-and-rural-communities/
 
 male_age_probabilities = [0.024, 0.205, 0.158, 0.101]
 female_age_probabilities = [0.023, 0.205, 0.164, 0.120]
 gender_age_probabilities = male_age_probabilities + female_age_probabilities
-
-# Get every combination of genders and age ranges using the cartesian product of the two subsets.
-# This will help us make the selection using a joint probability mass function using `numpy.random.choice`.
-
-gender_age_choices = [
-    gender | age for gender, age in
-    itertools.product(Demographics.genders(), Demographics.age_ranges())
-]
-
-# Source:
-# https://www.pewresearch.org/social-trends/2018/05/22/demographic-and-economic-trends-in-urban-suburban-and-rural-communities/
-
 location_probabilities = [0.31, 0.55, 0.14]
 
+# Get every combination of genders, age ranges, and locations using the cartesian product of the subsets.
+# This will help us make the selection using a joint probability mass function using `numpy.random.choice`.
 
-def generate_population(n: int) -> np_type.NDArray[Demographics]:
-    gender_ages = np.random.choice(gender_age_choices, n, p=gender_age_probabilities)
-    locations = np.random.choice(Demographics.locations(), n, p=location_probabilities)
+choices = [
+    gender | age | location
+    for gender, age, location in itertools.product(
+        Demographics.genders(), Demographics.age_ranges(), Demographics.locations()
+    )
+]
+probabilities = [a * b for a, b in itertools.product(gender_age_probabilities, location_probabilities)]
 
-    return gender_ages | locations
+# Some configuration options to rig/tune the election.
 
+POPULATION_SIZE = 320_000_000
+
+# Where the magic all happens
 
 if __name__ == "__main__":
-    print(generate_population(1_000_000))
+    start = time.perf_counter()
+    population = np.random.choice(choices, POPULATION_SIZE, p=probabilities)
+    print(f"Generated {len(population)} people in {time.perf_counter() - start}s")
